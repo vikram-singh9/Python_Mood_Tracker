e9933cd76b6db9c7cf6bc20f544c198b8aad19c7
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-05 03:02:08 +0500

    updated

--- mood-tracker-app/main.py
+++ mood-tracker-app/main.py
@@ -12,8 +12,32 @@ def load_mood_data():
     return pd.read_csv(MOOD_FILE)
 
 def save_mode_data(mood,date):
-    with open(MOOD_FILE, "a") as file
+    with open(MOOD_FILE, "a") as file:
+        writer = csv.writer(file)
+        writer.writerow([date, mood])
 
-    writer = csv.writer(file)
 
-    writer.writerow([date, mood])
+st.title("Mood Tracker App")
+
+today = datetime.date.today()
+
+st.subheader("How are you feeling today?")
+
+mood = st.selectbox("Select you mood", ["Happy", "Sad", "Angry", "Excited", "Bored", "Anxious"])
+
+if st.button("Log Mood"):
+    save_mode_data(today, mood)
+    st.success("mood logged successfully!")
+    
+
+data = load_mood_data()
+
+
+if not data.empty:
+    st.subheader("Mood changes over time")
+
+    data["Date"] = pd.to_datetime(data["Date"])
+
+    mood_counts = data.groupby("Mood").count()["Date"]
+
+    st.bar_chart(mood_counts)
