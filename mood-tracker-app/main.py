f3ac99f54babde2ea8adac6deacbbdae2d2a42fe
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-05 02:11:18 +0500

    Update main.py

--- mood-tracker-app/main.py
+++ mood-tracker-app/main.py
@@ -10,3 +10,10 @@ def load_mood_data():
     if not os.path.exists(MOOD_FILE):
         return pd.DataFrame(columns=["Date", "Mood"])
     return pd.read_csv(MOOD_FILE)
+
+def save_mode_data(mood,date):
+    with open(MOOD_FILE, "a") as file
+
+    writer = csv.writer(file)
+
+    writer.writerow([date, mood])
